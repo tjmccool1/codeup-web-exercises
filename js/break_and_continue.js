0a0b48e45e78b43b9141d78e290f106367e9f297
TMcCool <tjmccool1@icloud.com>
2018-01-31 15:31:38 -0600

    just caching up git

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,11 +1,30 @@
 // break and continue
 
 
-//var allCones = Math.floor(Math.random() * 50) + 50;
+//var allCones = Math.floor(Math.random() * 50) + 1;
 
+
+// Prompt the user for an odd number between 1 and 50.
 var input;
-do{
-    prompt("please inout a number between 1 -50");
-}  while ((input > 50 || input < 1)|| isNaN(parseFloat(number)));
-console.log("Number to skip is" + input)
-for(var i = 1; i <= 50; i++)
+
+    do {
+         input = Math.floor(Math.random() * 50) + 1;
+         console.log(input)
+       // input = prompt("Please input a number between 1 and 50");
+    } while(input > 50 || input < 1 || isNaN(parseFloat(input)) || input % 2 === 0);
+
+    +console.log("number to skip is " + input);
+
+    // Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+        for(var i = 1; i <= 50; i++) {
+        if(i % 2 === 0) {
+               continue;
+            }
+       if(i === input) {
+                console.log("Yikes! Skipping the number " + i);
+               continue;
+            }
+
+            console.log("Here is an odd number " + i);
+
+        }
--- js/for_loops.js
+++ js/for_loops.js
@@ -39,7 +39,13 @@
     // }
 
 
-     
+     // function numPyramid (num) {
+     //     for (let i = 1; i <= num; i++) {
+     //         console.log((!i) ? 1 : '0'.repeat(i));
+     //     }
+     //     return 'Complete';
+     // }
+     // console.log(numPyramid(10));
 
 
 
