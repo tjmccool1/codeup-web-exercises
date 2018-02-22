9d261cc2a65fadd885d367328eb105942a233d7f
TMcCool <tjmccool1@icloud.com>
2018-02-22 10:37:44 -0600

    updated promises exercise.

--- js/promises.js
+++ js/promises.js
@@ -22,23 +22,23 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 // Create a function that accepts a github username, and returns a promise that resolves with the date of the last commit that user made. Reference the github api documentation to achieve this.
 
-function getCommits(username) {
-
-    let gitHub = fetch( `https://api.github.com/users/${username}/events`, { headers: { 'Authorization': 'aebb4ad9023b4aeeb1b276291b8365b6dd5656cf' } } );
-
-    gitHub.then( (response) => {
-        response.json().then( (response) => {
-            let gitHub = response.filter( (v) => v.type === 'PushEvent' )[0];
-            console.log( `${username}'s Last Commit: "${gitHub.payload.commits[0].message}" at ${gitHub.created_at}` );
-        });
-    });
-
-    gitHub.catch( (response) => {
-        console.log(response);
-    });
-}
-
-getCommits('tjmccool1');
+// function getCommits(username) {
+//
+//     let gitHub = fetch( `https://api.github.com/users/${username}/events`, { headers: { 'Authorization': 'aebb4ad9023b4aeeb1b276291b8365b6dd5656cf' } } );
+//
+//     gitHub.then( (response) => {
+//         response.json().then( (response) => {
+//             let gitHub = response.filter( (v) => v.type === 'PushEvent' )[0];
+//             console.log( `${username}'s Last Commit: "${gitHub.payload.commits[0].message}" at ${gitHub.created_at}` );
+//         });
+//     });
+//
+//     gitHub.catch( (response) => {
+//         console.log(response);
+//     });
+// }
+//
+// getCommits('tjmccool1');
 
 //3rd aebb4ad9023b4aeeb1b276291b8365b6dd5656cf
 
@@ -46,9 +46,26 @@ getCommits('tjmccool1');
 
 //1st  68a73410bba48addaa580a58aa35d12a117f84dc
 
-function githubCheck() {
-    const gHPromise = fetch('https://api.github.com/users', {headers: {'Authorization': 'aebb4ad9023b4aeeb1b276291b8365b6dd5656cf'}});
-    return gHPromise;
-}
+// function githubCheck() {
+//     const gHPromise = fetch('https://api.github.com/users', {headers: {'Authorization': 'aebb4ad9023b4aeeb1b276291b8365b6dd5656cf'}});
+//     return gHPromise;
+// }
+//
+// console.log(githubCheck());
 
-console.log(githubCheck());
+function gitLastCommit(username) {
+    let dates = [];
+
+    fetch(`https://api.github.com/users/${username}/repos?per_page=100`, {headers: {'Authorization': '68a73410bba48addaa580a58aa35d12a117f84dc'}})
+        .then(response => response.json())
+        .then(repositories => {
+            repositories.map(repo => {
+                dates.push(repo.updated_at);
+            });
+            console.log(dates);
+            dates.sort();
+            document.querySelector('span').innerText = new Date(dates[dates.length - 1]);
+
+        });
+}
+gitLastCommit('tjmccool1');
